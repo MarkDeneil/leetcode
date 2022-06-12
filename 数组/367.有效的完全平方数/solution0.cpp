9378f3e473712101d0c3b67d54fba5235d22ece5
MarkDeneil@github <83362131+MarkDeneil@users.noreply.github.com>
2022-06-12 17:13:15 +0800

    Update solution0.cpp

--- 数组/367.有效的完全平方数/solution0.cpp
+++ 数组/367.有效的完全平方数/solution0.cpp
@@ -1,1 +1,21 @@
-// 参考 
+// 参考 69.x的平方根
+// 二分法
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long l = 0;
+        long long r = num;
+        if (num == 1) return true;
+        while ( l <= r)
+        {
+            long long mid = l + (r - l) / 2;
+            if ((long long)mid * mid == num)
+                return true;
+            else if ((long long)mid * mid < num)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return false;
+    }
+};
